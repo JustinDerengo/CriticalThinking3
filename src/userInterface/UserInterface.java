c6f8cfded4db701ae38d45a046ef760edef9a410
Justin <138933736+JustinDerengo@users.noreply.github.com>
2023-09-02 22:06:40 -0600

    changeBackgroundColor method

--- src/userInterface/UserInterface.java
+++ src/userInterface/UserInterface.java
@@ -41,12 +41,25 @@ public class UserInterface extends JFrame {
         
 	}
 	
+	/*
+	 * this method will create a new date and time format and append it to the text area
+	 * @param:none
+	 * @return:void
+	 * @throws:none
+	 */
 	private void printDateTime() {
 		SimpleDateFormat dateFormat = new SimpleDateFormat ("yyyy-MM-dd HH:mm:ss");
 		String dateTime = dateFormat.format(new Date());
 		textArea.append(dateTime + "\n");
 	}
 	
+	/*
+	 * this method will try to create a new log.txt file using FileWriter, BufferedWriter, and PrintWriter
+	 * and will display a message when it is done, or failed
+	 * @param:none
+	 * @return:void
+	 * @throws:error saving to file
+	 */
 	private void saveToFile() {
 		try (FileWriter writer = new FileWriter("log.txt", true);
 				BufferedWriter bufferedWriter = new BufferedWriter(writer);
@@ -59,5 +72,19 @@ public class UserInterface extends JFrame {
 		}
 	}
 	
-	
+	/*
+	 * this method will change the background color of the text field to a random hue of orange
+	 *  on a scale from red to yellow using Math.random and Color object
+	 *  @param:none
+	 *  @return:none
+	 *  @throws:none
+	 */
+	private void changeBackgroundColor() {
+		int red = 255;
+		int green = (int) (Math.random() *256);
+		int blue = 0;
+		
+		Color randomOrangeHue = new Color(red, green, blue);
+		textArea.setBackground(randomOrangeHue);
+	}
 }
