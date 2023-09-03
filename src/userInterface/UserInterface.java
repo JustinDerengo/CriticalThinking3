dccfc2511845e7ce1d77c6c5ee687c972058dac0
Justin <138933736+JustinDerengo@users.noreply.github.com>
2023-09-02 21:31:08 -0600

    intialize and import

--- src/userInterface/UserInterface.java
+++ src/userInterface/UserInterface.java
@@ -1,5 +1,17 @@
 package userInterface;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 public class UserInterface {
 
+	private JTextArea textArea;
+	private JMenu menu;
+	private Color initialBackgroundColor;
+	
+	
+	
 }
